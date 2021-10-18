53c5356a23a25e62b0142434a673075b0cb703db
Grant <GrantWils.23@googlemail.com>
2021-10-18 18:25:08 +0000

    add game instruction modal and question-counter elements  and start adding color and design to website

---
+++ assets/js/scripts.js
@@ -0,0 +1,29 @@
+
+
+// GAME INSTRUCTIONS HELP MODAL // 
+
+// Get the modal
+var modal = document.getElementById("gameHelp");
+
+// Get the button that opens the modal
+var btn = document.getElementById("gameInstructions");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
