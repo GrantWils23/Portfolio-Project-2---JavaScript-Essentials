81e4b8716a3657e2632041b993bcfaa6ac4a5cf5
Grant <GrantWils.23@googlemail.com>
2021-10-21 21:22:02 +0000

    restructure the questions to have a boolean value instead of a correct key,value

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -34,281 +34,253 @@ window.onclick = function(event) {
 var quizQuestions = [
   {
     question: "What is the Capital of Germany?",
-    answers: {
-      a: "Paris",
-      b: "Amsterdam",
-      c: "Budapest",
-      d: "Berlin",
-    },
-    correctAnswer: "d",
+    answers: [
+      {a: "Paris", correct: false},
+      {b: "Amsterdam", correct: false},
+      {c: "Budapest", correct: false},
+      {d: "Berlin", correct: true}
+    ]
   },
   {
     question: "What town do the Simspons live in?",
-    answers: {
-      a: "Springfield",
-      b: "Quahog",
-      c: "New York",
-      d: "Atlantas",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "Springfield", correct: true},
+      {b: "Quahog", correct: false},
+      {c: "New York", correct:false},
+      {d: "Atlantas", correct: false}
+    ]
   },
   {
     question: "What is Superman's weakness?",
-    answers: {
-      a: "Sausages",
-      b: "Kryptonite",
-      c: "Working at the Daily Planet",
-      d: "Samsonite",
-    },
-    correctAnswer: "b",
+    answers: [
+      {a: "Sausages", correct: false},
+      {b: "Kryptonite", correct: true},
+      {c: "Working at the Daily Planet", correct: false},
+      {d: "Samsonite", correct: false}
+    ]
   },
   {
     question: "What does LED stand for?",
-    answers: {
-      a: "Light Emitting Diode",
-      b: "Light Emitting Danger",
-      c: "Light Energy Detected",
-      d: "Light Emitting Disc",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "Light Emitting Diode", correct: true},
+      {b: "Light Emitting Danger", correct: false},
+      {c: "Light Energy Detected", correct: false},
+      {d: "Light Emitting Disc", correct: false}
+    ]
   },
   {
     question: "Who helped co-write the lyrics to the hit song from Dire Straits 'Money for Nothing'?",
-    answers: {
-      a: "Neil Diamond",
-      b: "Bernie Taupe",
-      c: "Sting",
-      d: "Elton John",
-    },
-    correctAnswer: "c",
+    answers: [
+      {a: "Neil Diamond", correct: false},
+      {b: "Bernie Taupe", correct: false},
+      {c: "Sting", correct: true},
+      {d: "Elton John", correct: false}
+    ]
   },
   {
     question: "From which country would you find the following items from? Kielbasa, Pierogi, Barszcz, Wodka?",
-    answers: {
-      a: "Tialand",
-      b: "Russia",
-      c: "Hungary",
-      d: "Poland",
-    },
-    correctAnswer: "d",
+    answers: [
+      {a: "Tialand",correct: false},
+      {b: "Russia", correct: false},
+      {c: "Hungary", correct: false},
+      {d: "Poland", correct: true}
+    ]
   },
   {
     question: "How many neighbours does Russia Share their borders with? ",
-    answers: {
-      a: "12",
-      b: "14",
-      c: "16",
-      d: "18",
-    },
-    correctAnswer: "b",
+    answers: [
+      {a: "12", correct: false},
+      {b: "14", correct: true},
+      {c: "16", correct: false},
+      {d: "18", correct: false}
+    ]
   },
   {
     question: "How old was Wolfgang Amadeus Mozart when he died?",
-    answers: {
-      a: "35",
-      b: "45",
-      c: "65",
-      d: "75",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "35", correct: true},
+      {b: "45", correct: false},
+      {c: "65", correct: false},
+      {d: "75", correct: false}
+    ]
   },
   {
     question: "What is the smallest Country in the World?",
-    answers: {
-      a: "San Marino",
-      b: "Monaco",
-      c: "Atlanta City",
-      d: "Vatican City",
-    },
-    correctAnswer: "d",
+    answers: [
+      {a: "San Marino", correct: false},
+      {b: "Monaco", correct: false},
+      {c: "Atlanta City", correct: false},
+      {d: "Vatican City", correct: true}
+    ]
   },
   {
     question: "What is the speed of light?",
-    answers: {
-      a: "200102 miles per second",
-      b: "186000 miles per second",
-      c: "148000 miles per second",
-      d: "198000 miles per second",
-    },
-    correctAnswer: "b",
+    answers: [
+      {a: "200102 miles per second", correct: false},
+      {b: "186000 miles per second", correct: true},
+      {c: "148000 miles per second", correct: false},
+      {d: "198000 miles per second", correct: false}
+    ]
   },
   {
     question: "What would you use a deburrer for?",
-    answers: {
-      a: "To smooths edges and sides of manufactured parts",
-      b: "To open canned products",
-      c: "To test humidity in the air",
-      d: "To dig a hole in the ground",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "To smooths edges and sides of manufactured parts", correct: true},
+      {b: "To open canned products", correct: false},
+      {c: "To test humidity in the air", correct: false},
+      {d: "To dig a hole in the ground", correct: false}
+    ]
   },
   {
     question: "Which snake is not a poisonous?",
-    answers: {
-      a: "Black Mamba",
-      b: "Inland Taipan",
-      c: "Boa Constrictor",
-      d: "Cobra",
-    },
-    correctAnswer: "d",
+    answers: [
+      {a: "Black Mamba", correct: false},
+      {b: "Inland Taipan", correct: false},
+      {c: "Boa Constrictor", correct: false},
+      {d: "Cobra", correct: true}
+    ]
   },
   {
     question: "Which snake is poisonous?",
-    answers: {
-      a: "Indian Krait",
-      b: "Burmese Python",
-      c: "Green Anaconda",
-      d: "Coin Snake",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "Indian Krait", correct: true},
+      {b: "Burmese Python", correct: false},
+      {c: "Green Anaconda", correct: false},
+      {d: "Coin Snake", correct: false}
+    ]
   },
   {
     question: "What is the tallest mountain on Earth?",
-    answers: {
-      a: "Mount Everest",
-      b: "Mount Rushmore",
-      c: "K2",
-      d: "Cho Oyu",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "Mount Everest", correct: true},
+      {b: "Mount Rushmore", correct:false},
+      {c: "K2", correct: false},
+      {d: "Cho Oyu", correct: false}
+    ]
   },
   {
     question: "What did Lamborghini make before it made Super Cars?",
-    answers: {
-      a: "Windows",
-      b: "Tractors",
-      c: "Furniture",
-      d: "Motorbikes",
-    },
-    correctAnswer: "b",
+    answers: [
+      {a: "Windows", correct: false},
+      {b: "Tractors", correct: true},
+      {c: "Furniture", correct: false},
+      {d: "Motorbikes", correct: false}
+    ]
   },
   {
     question: "How many litres are in a gallon?",
-    answers: {
-      a: "3.90",
-      b: "5.12",
-      c: "4.55",
-      d: "4.23",
-    },
-    correctAnswer: "c",
+    answers: [
+      {a: "3.90", correct: false},
+      {b: "5.12", correct: false},
+      {c: "4.55", correct: true},
+      {d: "4.23", correct: false}
+    ]
   },
   {
     question: "Which English Football Team went a whole season unbeaten in the Premier League?",
-    answers: {
-      a: "Arsenal",
-      b: "Manchester United",
-      c: "Manchester City",
-      d: "Chelsea",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "Arsenal", correct: true},
+      {b: "Manchester United", correct: false},
+      {c: "Manchester City", correct: false},
+      {d: "Chelsea", correct: false}
+    ]
   },
   {
     question: "How many Grand Slams has Roger Federer won up till 2021?",
-    answers: {
-      a: "22",
-      b: "21",
-      c: "20",
-      d: "23",
-    },
-    correctAnswer: "c",
+    answers: [
+      {a: "22", correct: false},
+      {b: "21", correct: false},
+      {c: "20", correct: true},
+      {d: "23", correct: false}
+    ]
   },
   {
     question: "How many legs does a ladybug have?",
-    answers: {
-      a: "4",
-      b: "8",
-      c: "10",
-      d: "6",
-    },
-    correctAnswer: "d",
+    answers: [
+      {a: "4", correct: false},
+      {b: "8", correct: false},
+      {c: "10", correct: true},
+      {d: "6", correc: false}
+    ]
   },
   {
     question: "Where can you find a timpani?",
-    answers: {
-      a: "Inside a car engine",
-      b: "In an orchestra",
-      c: "mixed in a bolognese sauce",
-      d: "Inside a jewellers tool box",
-    },
-    correctAnswer: "b",
+    answers: [
+      {a: "Inside a car engine", correct: false},
+      {b: "In an orchestra", correct: true},
+      {c: "mixed in a bolognese sauce", correct: false},
+      {d: "Inside a jewellers tool box", correct: false}
+    ]
   },
   {
     question: "How many lives does a cat have?",
-    answers: {
-      a: "One",
-      b: "None",
-      c: "Nine",
-      d: "Twentyfour",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "One", correct: true},
+      {b: "None", correct: false},
+      {c: "Nine", correct: false},
+      {d: "Twentyfour", correct: false}
+    ]
   },
   {
     question: "Who Played James Bond in 'You Only Live Twice'?",
-    answers: {
-      a: "Roger Moore",
-      b: "Sean Connery",
-      c: "Piers Brosnan",
-      d: "Daniel Craig",
-    },
-    correctAnswer: "b",
+    answers: [
+      {a: "Roger Moore", correct: false},
+      {b: "Sean Connery", correct: true},
+      {c: "Piers Brosnan", correct: false},
+      {d: "Daniel Craig", correct: false}
+    ]
   },
   {
     question: "What film is the character 'Doctor Evil' well known from?",
-    answers: {
-      a: "The Fantastic Four",
-      b: "Dr No",
-      c: "Austin Powers",
-      d: "Dispicable Me",
-    },
-    correctAnswer: "c",
+    answers: [
+      {a: "The Fantastic Four", correct: false},
+      {b: "Dr No", correct: false},
+      {c: "Austin Powers", correct: true},
+      {d: "Dispicable Me", correct: false}
+    ]
   },
   {
     question: "When did the UK fully ban the use of Asbestos?",
-    answers: {
-      a: "1970",
-      b: "1980",
-      c: "1990",
-      d: "2000",
-    },
-    correctAnswer: "d",
+    answers: [
+      {a: "1970", correct: false},
+      {b: "1980", correct: false},
+      {c: "1990", correct: false},
+      {d: "2000", correct: true}
+    ]
   },
   {
     question: "Who created the character 'Winnie the Pooh'?",
-    answers: {
-      a: "A.A. Milne",
-      b: "Walt Disney",
-      c: "Michael Bond",
-      d: "Mary Tourtel",
-    },
-    correctAnswer: "a",
+    answers: [
+      {a: "A.A. Milne", correct: true},
+      {b: "Walt Disney", correct: false},
+      {c: "Michael Bond", correct: false},
+      {d: "Mary Tourtel", correct: false}
+    ]
   },
   {
     question: "How many times has the TV show 'Family Guy' been cancelled?",
-    answers: {
-      a: "0",
-      b: "2",
-      c: "1",
-      d: "3",
-    },
-    correctAnswer: "b",
+    answers: [
+      {a: "0", correct: false},
+      {b: "2", correct: true},
+      {c: "1", correct: false},
+      {d: "3", correct: false},
+    ]
   },
   {
     question: "Who was supposed to play the T-1000 in 'Terminator 2; Judgement Day' before Robert Patrick took up the role?",
-    answers: {
-      a: "Bill Murray",
-      b: "Carl Weathers",
-      c: "Billy Idol",
-      d: "Jean Claude Van Damme",
-    },
-    correctAnswer: "c",
+    answers: [
+      {a: "Bill Murray", correct: false}, 
+      {b: "Carl Weathers", correct: false},
+      {c: "Billy Idol", correct: true},
+      {d: "Jean Claude Van Damme", correct: false}
+    ]
   },
 ]
 
 
 
 const startButton = document.getElementById("start-game");
-startButton.addEventListener("click", startGame)
-
+startButton.addEventListener("click", startGame);
 
 
 let mutableList = quizQuestions;
@@ -330,10 +302,10 @@ function startGame() {
     randomQuestions.push(striptedQuestion); // push the stripped uestion into the list of 10 questions for the game;
     i++;
   };
-  console.log(randomQuestions)
+  console.log(randomQuestions);
   displayQuestion();
 
-}
+};
 
 
 
@@ -359,11 +331,11 @@ function displayQuestion() {
 
   let questionNumber = document.getElementById("question-number");
   questionNumber.innerHTML = currentQuestionNumberIndex;
-}
+};
 
 
 // SUBMIT ANSWER & TO NEXT QUESTION//
-function submitAnswer() {
+function submitAnswer(event) {
   let buttons = document.getElementsByTagName("button");
   for (let button of buttons) {
     button.addEventListener("click", function() {
@@ -374,12 +346,21 @@ function submitAnswer() {
   }
 
   currentQuestionNumberIndex++;
-}
+};
 
-//CHECK WHEN REACHED 10 QUESIIONS THAT WILL END GAME AND WILL RETURN THE SCORE;
-function checkAnswer() {
-  
-}
+// ANSWER BOX SELECTED
+// function answerBoxSelected(event) {
+
+// } 
+
+// CHECK WHEN REACHED 10 QUESIIONS THAT WILL END GAME AND WILL RETURN THE SCORE;
+// function checkAnswer(event) {
+//   let button
+//   if (this.data-type === question.correctAnswer) {
+
+//   }
+
+// }
 
 
 // KEEP TRACK OF QUESTION NUMBER //
